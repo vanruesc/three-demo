6381cc15b917119ef9c3f8fba4f60c858b695afc
Raoul v. R <vanruesc@users.noreply.github.com>
2018-07-01 21:19:17 +0200

    Revised the build setup.

--- rollup.config.js
+++ rollup.config.js
@@ -15,24 +15,15 @@ const lib = {
 
 	input: pkg.module,
 	output: {
-		file: "build/bundle.js",
+		file: "build/" + pkg.name + ".js",
 		format: "umd",
 		name: pkg.name.replace(/-/g, "").toUpperCase(),
 		banner: banner,
-		globals: {
-			"three": "THREE"
-		}
+		globals: { three: "THREE" }
 	},
 
 	external: ["three"],
-
-	plugins: [resolve()].concat(process.env.BABEL_ENV === "production" ?
-		[babel(), minify({
-			bannerNewLine: true,
-			sourceMap: false,
-			comments: false
-		})] : []
-	)
+	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
 
 };
 
@@ -42,20 +33,42 @@ const demo = {
 	output: {
 		file: "public/demo/index.js",
 		format: "iife",
-		globals: {
-			"three": "THREE"
-		}
+		globals: { three: "THREE" }
 	},
 
 	external: ["three"],
-
-	plugins: [resolve()].concat(process.env.BABEL_ENV === "production" ?
-		[babel(), minify({
-			sourceMap: false,
-			comments: false
-		})] : []
-	)
+	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
 
 };
 
-export default [lib, demo];
+export default [lib, demo].concat((process.env.NODE_ENV === "production") ? [
+
+	Object.assign({}, lib, {
+
+		output: Object.assign({}, lib.output, {
+			file: "build/" + pkg.name + ".min.js"
+		}),
+
+		plugins: [resolve()].concat([babel(), minify({
+			bannerNewLine: true,
+			sourceMap: false,
+			comments: false
+		})])
+
+	}),
+
+	Object.assign({}, demo, {
+
+		output: Object.assign({}, demo.output, {
+			file: "public/demo/index.min.js"
+		}),
+
+		plugins: [resolve()].concat([babel(), minify({
+			bannerNewLine: true,
+			sourceMap: false,
+			comments: false
+		})])
+
+	})
+
+] : []);
