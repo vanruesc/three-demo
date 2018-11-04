4dd324d2f37a40625e3e838d63ca8639bfdcef34
Raoul v. R <vanruesc@users.noreply.github.com>
2018-11-04 13:13:44 +0100

    Updated doc comments.

--- src/demo-manager-events.js
+++ src/demo-manager-events.js
@@ -3,7 +3,8 @@ import { DemoManagerEvent } from "./DemoManagerEvent.js";
 /**
  * A demo manager change event.
  *
- * This event is dispatched by {@link DemoManager}.
+ * This event is dispatched by {@link DemoManager} when the user switches to
+ * another demo.
  *
  * @type {DemoManagerEvent}
  * @example demoManager.addEventListener("change", myListener);
@@ -14,7 +15,8 @@ export const change = new DemoManagerEvent("change");
 /**
  * A demo manager load event.
  *
- * This event is dispatched by {@link DemoManager}.
+ * This event is dispatched by {@link DemoManager} when a demo has finished
+ * loading and is about to start rendering.
  *
  * @type {DemoManagerEvent}
  * @example demoManager.addEventListener("load", myListener);
