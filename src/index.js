9e4c4defbe6071c481deb89789f62f3a0bba0138
Raoul v. R <vanruesc@users.noreply.github.com>
2019-11-01 11:59:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-/**
- * Core components.
- *
- * @module three-demo
- */
-
 export { Demo } from "./Demo.js";
 export { DemoManager } from "./DemoManager.js";
 export { DemoManagerEvent } from "./DemoManagerEvent.js";
