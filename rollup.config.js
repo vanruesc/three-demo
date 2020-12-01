f5717e8b89546e3a09fd9dacf976d884dcb881a7
Raoul v. R <vanruesc@users.noreply.github.com>
2020-12-01 23:03:32 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
-import typescript  from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
+import esbuild  from "rollup-plugin-esbuild";
+import pkg from "./package.json";
 
-const pkg = require("./package.json");
 const date = (new Date()).toDateString();
+const production = (process.env.NODE_ENV === "production");
+const external = Object.keys(pkg.peerDependencies);
+const globals = Object.assign({}, ...external.map((value) => ({
+	[value]: value.replace(/-/g, "").toUpperCase()
+})));
 
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date}
@@ -13,76 +18,56 @@ const banner = `/**
  * @license ${pkg.license}
  */`;
 
-const production = (process.env.NODE_ENV === "production");
-const external = Object.keys(pkg.peerDependencies);
-const globals = Object.assign({}, ...external.map((value) => ({
-	[value]: value.replace(/-/g, "").toUpperCase()
-})));
+const ext = new Map([
+	["dts", ".d.ts"],
+	["esm", ".esm.js"],
+	["umd", ".js"],
+	["iife", ".js"],
+	["min", ".min.js"]
+]);
 
-const lib = {
-	module: {
-		input: "src/index.ts",
-		plugins: [resolve(), typescript({
-			tsconfig: "src/tsconfig.json"
-		})],
-		external,
-		output: {
-			dir: "build",
-			entryFileNames: pkg.name + ".esm.js",
-			format: "esm",
-			banner
-		}
-	},
-	main: {
-		input: "src/index.ts",
-		plugins: [
+function bundle(id, format, minify = false) {
+
+	const lib = (id === "lib");
+	const transpile = (format !== "esm" && (minify || production));
+	const extKey = minify ? "min" : format;
+	const fileName = lib ? pkg.name : "index";
+	const outDir = lib ? "build" : "public/demo";
+
+	return {
+		input: lib ? "src/index.ts" : "demo/src/index.ts",
+		external: lib ? external : null,
+		plugins: (format === "dts") ? [dts()] : [
 			resolve(),
-			typescript({
-				tsconfig: "src/tsconfig.json"
-			}),
+			esbuild({
+				tsconfig: lib ? "src/tsconfig.json" : "tsconfig.json",
+				target: "esnext",
+				minify
+			})
+		].concat(transpile ? [
 			babel({
 				babelHelpers: "bundled",
-				extensions: [".ts"]
+				extensions: [".js", ".ts"]
 			})
-		],
-		external,
-		output: [{
-			dir: "build",
-			entryFileNames: pkg.name + ".js",
-			format: "umd",
+		] : []),
+		output: {
+			file: `${outDir}/${fileName}${ext.get(extKey)}`,
 			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			banner: lib ? banner : null,
 			globals,
-			banner
-		}, {
-			dir: "build",
-			entryFileNames: pkg.name + ".min.js",
-			format: "umd",
-			name: pkg.name.replace(/-/g, "").toUpperCase(),
-			plugins: [terser()],
-			globals,
-			banner
-		}]
-	}
-};
+			format
+		}
+	};
 
-const demo = {
-	input: "demo/src/index.ts",
-	plugins: [resolve(), typescript({
-		tsconfig: "demo/src/tsconfig.json"
-	})].concat(!production ? [] : [babel({
-		babelHelpers: "bundled",
-		extensions: [".ts"]
-	})]),
-	output: [{
-		dir: "public/demo",
-		entryFileNames: "[name].js",
-		format: "iife"
-	}].concat(production ? [{
-		dir: "public/demo",
-		entryFileNames: "[name].min.js",
-		format: "iife",
-		plugins: [terser()]
-	}] : [])
-};
+}
 
-export default production ? [lib.module, lib.main, demo]: [demo];
+export default production ? [
+	bundle("lib", "dts"),
+	bundle("lib", "esm"),
+	bundle("lib", "umd"),
+	bundle("lib", "umd", true),
+	bundle("demo", "iife"),
+	bundle("demo", "iife", true)
+] : [
+	bundle("demo", "iife")
+];
