fd890571a17a07a47ec9310b00c5ef3467bed685
Raoul v. R <vanruesc@users.noreply.github.com>
2021-01-28 15:04:43 +0100

    Update esbuild.ts

--- esbuild.ts
+++ esbuild.ts
@@ -2,54 +2,54 @@ import { BuildOptions, BuildResult, startService } from "esbuild";
 import { watch } from "chokidar";
 import * as path from "path";
 import * as yargs from "yargs";
-import configs from "./esbuild.config";
+import { configLists, sourceDirectories } from "./esbuild.config";
 
 const { argv } = yargs.options({ watch: { alias: "w", type: "boolean" } });
-
-function handleResult(result: BuildResult, file: string, elapsed: number): void {
-
-	console.log(`Built ${file} in ${elapsed}ms`);
-	result.warnings.forEach((w) => console.warn(w.text,
-		`${w.location.file} ${w.location.line}:${w.location.column}`));
-
-}
 
 async function build(changedFile: string = null): Promise<void> {
 
 	const service = await startService();
 	const f = (changedFile !== null) ? path.normalize(changedFile) : null;
-	const t0 = Date.now();
 
-	const promises: Promise<BuildResult>[] = configs.map((c: BuildOptions) => {
+	for(const configs of configLists) {
 
-		let p: Promise<BuildResult> = null;
+		const t0 = Date.now();
+		const promises: Promise<void>[] = configs.map((c: BuildOptions) => {
 
-		if(path.normalize(c.outfile) !== f) {
+			let p: Promise<void> = null;
 
-			p = service.build(c);
-			p.then((r) => handleResult(r, c.outfile, Date.now() - t0));
+			if(path.normalize(c.outfile) !== f) {
 
-		}
+				p = service.build(c).then((result) => {
 
-		return p;
+					console.log(`Built ${c.outfile} in ${Date.now() - t0}ms`);
+					result.warnings.forEach((w) => console.warn(w.text,
+						`${w.location.line}:${w.location.column} ${w.location.file}`));
 
-	});
+				}).catch((e) => console.error(`Failed to build ${c.outfile}`));
 
-	Promise.all(promises)
-		.catch((e) => console.error(e))
-		.finally(() => service.stop());
+			}
+
+			return p;
+
+		});
+
+		await Promise.all(promises);
+
+	}
+
+	service.stop();
 
 }
 
 if(argv.watch) {
 
-	const entryPoints: string[] = configs.reduce((a: string[], b: BuildOptions) => a.concat(b.entryPoints), []);
-	const directories = [...new Set(entryPoints.map((f: string) => path.dirname(f)))];
-	console.log(`Watching ${directories.join(", ").replace(/,\s([^,]*)$/, " and $1")} for changes…\n`);
+	console.log("Watching %s for changes…\n",
+		sourceDirectories.join(", ").replace(/, ([^,]*)$/, " and $1"));
 
-	const watcher = watch(directories);
+	const watcher = watch(sourceDirectories);
 	watcher.on("change", (f: string) => void build(f));
 
 }
 
-void build(null);
+void build();
